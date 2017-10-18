894351881f3ee69f8a24d855b181f09f67d99a6e
KUO <jkgace@gmail.com>
2017-10-17 21:59:21 -0400

    fixed julian no being a c-x/c-v wizard

--- menus.lua
+++ menus.lua
@@ -3,7 +3,7 @@ local server = require "server"
 
 menus = {}
 
-menus[1] = {buttons = {{x = 352, y = 275, w = 96, h = 36, txt = "server", func = create_server, args = {}}, {x = 452, y = 275, w = 96, h = 36, txt = "client", func = create_client, args = {}}},
+menus[1] = {buttons = {{x = 352, y = 275, w = 96, h = 36, txt = "server", func = server.init, args = {}}, {x = 452, y = 275, w = 96, h = 36, txt = "client", func = client.init, args = {}}},
             textboxes = {{x = 252, y = 275, w = 96, h = 16, table = ip, index = "ip", sampletxt = "I.P."}, {x = 252, y = 295, w = 96, h = 16, table = ip, index = "port", sampletxt = "Port", num = true}}}
 menus[2] = {}
 menus[3] = {}
