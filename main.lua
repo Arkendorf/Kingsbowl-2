e00c5b86bc934ab6b8a1b7d284b1f0bbd7f7de17
Arkendorf <amonafk@yahoo.com>
2017-10-28 18:29:12 -0400

    uncommented main

--- main.lua
+++ main.lua
@@ -10,49 +10,52 @@ keydowntable['1'] = server.init
 keydowntable['2'] = client.init
 
 love.load = function()
-  -- font = love.graphics.newImageFont("font.png",
-  --   " ABCDEFGHIJKLMNOPQRSTUVWXYZ" ..
-  --   "abcdefghijklmnopqrstuvwxyz" ..
-  --   "0123456789!?.:", 1)
-  -- love.graphics.setFont(font)
-  -- state.game = "menu"
-  -- state.gui = gui.new(menus[1])
-  -- game.load()
+  font = love.graphics.newImageFont("font.png",
+    " ABCDEFGHIJKLMNOPQRSTUVWXYZ" ..
+    "abcdefghijklmnopqrstuvwxyz" ..
+    "0123456789!?.:", 1)
+  love.graphics.setFont(font)
+  state.game = "menu"
+  state.gui = gui.new(menus[1])
+  game.load()
 end
 
 love.update = function(dt)
-  -- if state.game == "server" then
-  --   server.update(dt)
-  -- elseif state.game == "client" then
-  --   client.update(dt)
-  -- elseif state.game == "menu" then
-  -- end
-  -- state.gui:update(dt)
-  game.update(dt)
+  if state.game == "server" then
+    server.update(dt)
+    game.update(dt)
+  elseif state.game == "client" then
+    client.update(dt)
+    game.update(dt)
+  elseif state.game == "menu" then
+  end
+  state.gui:update(dt)
 end
 
 love.draw = function()
-  -- state.gui:draw()
-  -- love.graphics.print(ip.ip..":"..ip.port)
-
-  game.draw()
+  if state.game == "menu" then
+    state.gui:draw()
+    love.graphics.print(ip.ip..":"..ip.port)
+  else
+    game.draw()
+  end
 end
 
 love.mousepressed = function(x, y, button)
-  -- state.gui:mousepressed(x, y, button)
+  state.gui:mousepressed(x, y, button)
 end
 
 love.textinput = function(t)
-  -- state.gui:textinput(t)
+  state.gui:textinput(t)
 end
 
 love.keypressed = function(key)
-  -- keyuptable[key]()
-  -- state.gui:keypressed(key)
+  keyuptable[key]()
+  state.gui:keypressed(key)
 end
 
 love.keyreleased = function(key)
-  -- keydowntable[key]()
+  keydowntable[key]()
 end
 
 love.joystickadded = function(x)
